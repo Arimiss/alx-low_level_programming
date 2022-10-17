c588979de24ce4eb51c7cdaeb244de6d7fc0c840
Akiode Mistura <akiodemistura2005@gmail.com>
2022-10-17 16:40:07 +0000

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sentence to the screen
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
